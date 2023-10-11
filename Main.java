eadb95f273bad113f2427a08ef0c97a3998674ea
Alessandro <sandroscro@libero.it>
2023-10-11 16:21:30 +0200

    Added Main and Updated README

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int x=100, y=2;
+        x=x++;
+        y=y++;
+        System.out.println("The moltiplication of "+x+" and "+y+" is " +x*y);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Op_3
+Scrivi un programma che contiene un metodo che date due variabili le incrementi di un valore scelto da te e le moltiplichi fra di loro.
